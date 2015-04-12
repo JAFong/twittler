1492db6912e593dacce332454c374956931e1042
Justin Fong <justinf715@gmail.com>
2015-04-12 13:03:52 -0700

    Added REFRESH button.

--- myjquery.js
+++ myjquery.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-	$('div').on('click', function() {
+	$('.tweet').on('click', 'div', function() {
 		$(this).text("Jquery Test");
+	});
+	$('.refresh').on('click', function() {
+		//$('div').css('color', 'red')
+		var index = streams.home.length - 1;
+		var tweet = streams.home[index];
+        var $tweet = $('<div></div>');
+        $tweet.text('@' + tweet.user + ': ' + tweet.message);
+        $tweet.appendTo($('.tweet'));
 	});
 });
