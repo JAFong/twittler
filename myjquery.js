bc0122dcde73308d5cb04d91eba1f350719723fa
Justin Fong <justinf715@gmail.com>
2015-04-12 15:26:09 -0700

    Fix REFRESH button.

--- myjquery.js
+++ myjquery.js
@@ -3,11 +3,16 @@ $(document).ready(function() {
 		$(this).text("Jquery Test");
 	});
 	$('.refresh').on('click', function() {
-		//$('div').css('color', 'red')
-		var index = streams.home.length - 1;
-		var tweet = streams.home[index];
+		var $tweets = $('.tweet');
+        $tweets.html('');
+
+        var index = streams.home.length - 1;
+        while(index >= 0){
+        var tweet = streams.home[index];
         var $tweet = $('<div></div>');
         $tweet.text('@' + tweet.user + ': ' + tweet.message);
         $tweet.appendTo($('.tweet'));
-	});
+        index -= 1;
+    }
+     });
 });
